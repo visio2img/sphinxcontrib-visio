215ff3516dfff1d4f2b0a0bc9df27f670ce7a385
Yuki Yasuda <mathyassu@gmail.com>
2014-09-02 10:15:35 +0900

    add setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+setup(
+    name='visioimg',
+    version='1.0.0',
+    author='Yassu',
+    author_email='yassumath@gmail.com',
+    url='https://github.com/yassu/VisioInRst',
+    description='Python reStructuredText directive for embedding visio image',
+    license='MIT',
+    packages=['visioimg'],
+    install_requires=[
+        'visio2img'
+    ],
+    classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Natural Language :: English',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    ]
+)
