ae2ec547df6e6b9564915de33290ea793bc77069
Takeshi KOMIYA <i.tkomiya@gmail.com>
2014-09-15 15:20:07 +0900

    Bump version

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from setuptools import setup
 
 setup(
     name='sphinxcontrib-visio',
-    version='1.0.1',
+    version='1.0.2',
     author='Yassu',
     author_email='yassumath@gmail.com',
-    url='https://github.com/yassu/sphinxcontrib-visio',
+    maintainer='Takeshi KOMIYA',
+    maintainer_email='i.tkomiya@gmail.com',
+    url='https://github.com/visio2img/sphinxcontrib-visio',
     description='Python reStructuredText directive for embedding visio image',
     license='MIT',
     packages=['sphinxcontrib'],
